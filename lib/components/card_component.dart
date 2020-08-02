b631bed2c570159fba10ad75c1f84c4591444a63
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-08-01 19:46:03 -0500

    Add note component to display the notes on the main page

---
+++ lib/components/card_component.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CardComponent extends StatelessWidget {
+  const CardComponent({
+    Key key,
+    this.title,
+    this.body,
+  }) : super(key: key);
+
+  final String title;
+  final String body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(1.0),
+        child: Card(
+          child: ListTile(
+            leading: Icon(Icons.note),
+            title: Text(title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+            subtitle: Text(body),
+          )
+        )
+    );
+  }
+}
+
+/*
+
+Text(title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0))
+
+class _CardComponentState extends State<CardComponent> {
+  @override
+  Widget build(BuildContext context) {
+    String text = 'hola';
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.album),
+                title: Text(''),
+                subtitle: Text(''),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+
+ */
