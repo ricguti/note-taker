66d1e43ac8e73cfc7ff816b1d406c4b8bcf344f0
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-08-09 14:15:53 -0500

    Remove setters because they were not used

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -3,10 +3,6 @@ class Note {
   String body;
 
   Note(this.title, this.body);
-
-  void setTitle(String title) => this.title = title;
-
-  void setContent(String content) => this.body = content;
 }
 
 final List<Note> notes = [];
