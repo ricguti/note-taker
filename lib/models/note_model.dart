67e4c24f4d71498120f465c8df45fb130a23ed34
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-08-09 19:24:26 -0500

    Add NotesModel ChangeNotifier

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,33 @@
+import 'package:flutter/material.dart';
+
 class Note {
   String title;
   String body;
 
   Note(this.title, this.body);
+}
+
+class NotesModel extends ChangeNotifier {
+  final List<Note> _notes = [];
+
+  void addNote(Note note) {
+    _notes.add(note);
+    notifyListeners();
+  }
+
+  void removeNote(int index) {
+    if(index >= 0 && index < _notes.length) {
+      _notes.removeAt(index);
+      notifyListeners();
+    }
+  }
+
+  void editNote(int index, Note note) {
+    if(index >= 0 && index < _notes.length) {
+      _notes[index] = note;
+      notifyListeners();
+    }
+  }
 }
 
 final List<Note> notes = [];
