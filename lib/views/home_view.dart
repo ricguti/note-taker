194250c076b3373422dc508c4bff1470c7a81cc6
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-08-09 22:29:43 -0500

    Extract repeated notes.getNote(index) call to variable

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -18,31 +18,32 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Consumer<NotesModel>(
+          builder: (context, notes, child) {
+            return notes.isEmpty() || notes.length() == 0
+                ? Text('Your notes will be displayed here')
+                : ListView.builder(
+                    itemCount: notes.length(),
+                    itemBuilder: (BuildContext context, int index) {
+                      Note note = notes.getNote(index);
+                      return _tapableCard(
+                          note.title,
+                          note.body,
+                          _navigateToNoteView(index));
+                    });
+          },
         ),
-        body: Center(
-          child: Consumer<NotesModel>(
-            builder: (context, notes, child) {
-              return notes.isEmpty() || notes.length() == 0
-                  ? Text('Your notes will be displayed here')
-                  : ListView.builder(
-                  itemCount: notes.length(),
-                  itemBuilder: (BuildContext context, int index) {
-                    return _tapableCard(
-                        notes.getNote(index).title,
-                        notes.getNote(index).body,
-                        _navigateToNoteView(index));
-                  });
-            },
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _navigateToNoteView(-1),
-          tooltip: 'Create Note',
-          child: Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _navigateToNoteView(-1),
+        tooltip: 'Create Note',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 
   GestureDetector _tapableCard(
